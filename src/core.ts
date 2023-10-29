644e5bf66cf605be4f4c0c2b5c9de37ba99003a7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-10-29 15:28:04 +0800

    wip: transform the typescript signature of inferred parse result

--- src/core.ts
+++ src/core.ts
@@ -1062,7 +1062,96 @@ function isSimpleType(type: string): boolean {
   return !!type.match(/^\w+$/)
 }
 
-export function inferFromSampleValue<T>(value: T): Parser<T> {
+function _checkType<T>(t: T) {}
+
+type InferFieldName<S> = S extends `${infer N}$enums`
+  ? InferFieldName<N>
+  : S extends `${infer N}$enum`
+  ? InferFieldName<N>
+  : S extends `${infer N}$nullable`
+  ? InferFieldName<N>
+  : S extends `${infer N}$null`
+  ? InferFieldName<N>
+  : S extends `${infer N}$optional`
+  ? InferFieldName<N>
+  : S extends `${infer N}?`
+  ? InferFieldName<N>
+  : S
+
+_checkType<InferFieldName<'role'>>('role')
+_checkType<InferFieldName<'role$enums'>>('role')
+_checkType<InferFieldName<'role$enum'>>('role')
+_checkType<InferFieldName<'user_id$nullable'>>('user_id')
+_checkType<InferFieldName<'user_id$null'>>('user_id')
+_checkType<InferFieldName<'type$optional'>>('type')
+_checkType<InferFieldName<'type?'>>('type')
+_checkType<InferFieldName<'type$null?'>>('type')
+_checkType<InferFieldName<'role$enums$null?'>>('role')
+_checkType<InferFieldName<'role$null$enums?'>>('role')
+
+type ExcludeOptionalFieldName<S> = S extends `${infer H}?${infer T}`
+  ? never
+  : S extends `${infer H}$optional${infer T}`
+  ? never
+  : S
+
+_checkType<ExcludeOptionalFieldName<'id' | 'status?'>>('id')
+// @ts-expect-error
+_checkType<ExcludeOptionalFieldName<'id' | 'status?'>>('status?')
+// @ts-expect-error
+_checkType<ExcludeOptionalFieldName<'id' | 'status?'>>('status')
+
+type PickOptionalFieldName<S> = S extends `${infer H}?${infer T}`
+  ? S
+  : S extends `${infer H}$optional${infer T}`
+  ? S
+  : never
+
+// @ts-expect-error
+_checkType<PickOptionalFieldName<'id' | 'status?'>>('id')
+_checkType<PickOptionalFieldName<'id' | 'status?'>>('status?')
+_checkType<PickOptionalFieldName<'id' | 'status$optional'>>('status$optional')
+
+type InferType<T> = T extends Array<infer E>
+  ? Array<InferType<E>>
+  : T extends Record<infer K extends keyof T, infer V>
+  ? Partial<Record<InferFieldName<K>, V>> &
+      Pick<T, ExcludeOptionalFieldName<keyof T>>
+  : T
+
+_checkType<InferType<{ id: number; role: 'admin' }>>({ id: 1, role: 'admin' })
+_checkType<InferType<{ id: number; role?: 'admin' }>>({ id: 1 })
+_checkType<InferType<{ id: number; role?: 'admin' }>>({ id: 1, role: 'admin' })
+// @ts-expect-error
+_checkType<InferType<{ id: number; role?: 'admin' }>>({ id: 'admin', role: 1 })
+_checkType<InferType<{ 'id': number; 'role?': 'admin' }>>({
+  id: 1,
+  role: 'admin',
+})
+_checkType<InferType<{ 'id': number; 'role?': 'admin' }>>({ id: 1 })
+// @ts-expect-error
+_checkType<InferType<{ 'id': number; 'role?': 'admin' }>>({ id: 'admin' })
+_checkType<number[]>([1])
+_checkType<{ id: 1 }[]>([{ id: 1 }])
+// @ts-expect-error
+_checkType<InferType<{ 'id': number; 'role?': 'admin' }>>({})
+_checkType<{ posts: { 'id': number; 'status?': 'active' }[] }>({
+  posts: [
+    {
+      id: 1,
+      status: 'active',
+    },
+  ],
+})
+_checkType<{ posts: { 'id': number; 'status?': 'active' }[] }>({
+  posts: [
+    {
+      id: 1,
+    },
+  ],
+})
+
+export function inferFromSampleValue<T>(value: T): Parser<InferType<T>> {
   if (typeof value == 'string')
     return string({ sampleValue: value }) as Parser<string> as Parser<T>
   if (typeof value == 'number')
@@ -1139,3 +1228,9 @@ export function inferFromSampleValue<T>(value: T): Parser<T> {
   }
   throw new Error('unsupported sample value: ' + JSON.stringify(value))
 }
+
+let parser_1 = inferFromSampleValue({ id: 1, status: 's' })
+parser_1.parse({}).status // string
+
+let parser_2 = inferFromSampleValue({ id: 1, status$optional: 's' })
+parser_2.parse({}).status // string | number | undefined
